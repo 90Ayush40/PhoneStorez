57a7702b69b98ec7140c6c4873692032ae002efa
90Ayush40 <ayushaggarwal9041@gmail.com>
2020-12-04 00:39:57 +0530

    second commit on store app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from'./components/ProductList';
-import Product from './components/Product';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
    <React.Fragment>
      <Navbar />
-     <Product />
-     <ProductList />
-     <Default />
-     <Cart />
-     <Details />
+     <Switch>
+       <Route path='/cart'>
+          <Cart />
+       </Route>
+       <Route path='/details'>
+         <Details />
+       </Route>
+       <Route exact path='/'>
+         <ProductList />
+       </Route>
+       <Route>
+         <Default />
+       </Route>
+     </Switch>
    </React.Fragment>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import Title from './Title';
+import {ProductConsumer} from '../context';
 import Product from './Product';
 
-function ProductList() {
-    return (
-        <div>
-            <Product />
-        </div>
-    )
-}
+export default class ProductList extends Component {
+    
+    render() {
 
-export default ProductList
+        // console.log(this.state.products)
+
+        return (
+            <React.Fragment>
+            <div className='py-5'>
+                <div className='container'>
+                    <Title 
+                    name='our'
+                    title='products' />
+                    {/* Products row */}
+                    <div className='row'>
+                        <ProductConsumer>
+                            {(value) => {
+                                return value.products.map( product => {
+                                    return <Product Key={product.id} product={product} />
+                                })
+                            }}
+                        </ProductConsumer>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+
+        )
+    }
+}
